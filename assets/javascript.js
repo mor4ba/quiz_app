9b08efa966d358a688cab6091f5e877c4d9bd971
Moritz Hoffmeyer <moritzhoffmeyer@icloud.com>
2023-02-24 20:36:55 +0100

    wip

--- assets/javascript.js
+++ assets/javascript.js
@@ -2,7 +2,7 @@ import { toggleAccordion } from "./components/toggleAccordion.js";
 import { bookmarkCard } from "./components/bookmarkCard.js";
 import { changeLightMode } from "./components/colorMode.js";
 
-window.addEventListener("DOMContentLoaded", (event) => {
+window.addEventListener("DOMContentLoaded", () => {
   console.log("DOM fully loaded and parsed");
   toggleAccordion();
   bookmarkCard();
